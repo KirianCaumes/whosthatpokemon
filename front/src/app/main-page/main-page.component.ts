a7ee0d32e9215ce4e40303209452b42dbeb90422
Caumes Kirian <kirian.caumes@ynov.com>
2020-03-05 14:57:19 +0100

    [#9-vérifier-véracité-nom-pokémon] vérifier-véracité-nom-pokémon

--- front/src/app/main-page/main-page.component.ts
+++ front/src/app/main-page/main-page.component.ts
@@ -1,63 +1,85 @@
-import { Component, OnInit } from '@angular/core';
-import { faSync, faEye, faCheck, faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { faSync, faEye, faCheck, faAngleUp, faCog} from '@fortawesome/free-solid-svg-icons';
 import { SimilarityService } from '../service/similarity/similarity.service'
+import { SettingsService } from '../service/settings/settings.service';
 
 @Component({
     selector: 'app-main-page',
     templateUrl: './main-page.component.html',
     styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, OnDestroy {
     faSync = faSync;
     faEye = faEye;
     faCheck = faCheck;
     faAngleUp = faAngleUp;
-
-    //Generations
-    gens = {
-        one: true,
-        two: true,
-        three: true,
-        four: true,
-        five: true,
-        six: true,
-        seven: true
-    }
-
-    //Lang
-    lang = "fr"
+    faCog = faCog;
 
     //Input value
     inputPokemon = ""
 
-    constructor(private similarityService: SimilarityService) { }
+    //Pokemon infos
+    pokemon = { "id_pokemon": 25, "translate": { "fr": "Pikachu", "en": null, "de": null, "ja": null, "ko": null, "ru": null, "zh-hans": null, "zh-hant": null }, "generation": 1, "is_enable": true }
+
+    isAnswerDisplayed: Boolean = false
+
+    message = {
+        found: "Trouvé !",
+        close: "Si proche !",
+        quiteClose: "Assez proche",
+        notClose: "Si loin...",
+        empty: "Vous devez entrer quelque chose !"
+    }
+
+    //Message displayed by score
+    messageDisplayed = ""
+
+    isModalOpen = false
+
+    constructor(private similarityService: SimilarityService, private settingsService: SettingsService) {
+    }
 
     ngOnInit() {
-        console.log(this.similarityService.getScore("123","1223"))
+    }
+
+    ngOnDestroy() {
+
     }
 
     //Handle chnage for pokemon input
     changeInputPokemon(ev) {
-        console.log(this.gens)
-    }
-
-    //Handle change generation selected
-    changeGen(gen, ev) {
-        this.gens[gen] = ev.target.checked
+        this.inputPokemon = ev.target.value
     }
 
     //Check if value is okay
     check() {
+        let score = this.similarityService.getScore(this.pokemon.translate[this.settingsService.getSettings().lang], this.inputPokemon)
 
+        if (score < 3) {
+            this.messageDisplayed = this.message.found
+            this.isAnswerDisplayed = true
+        } else if (score < 5) {
+            this.messageDisplayed = this.message.close
+        } else if (score < 7) {
+            this.messageDisplayed = this.message.quiteClose
+        } else {
+            this.messageDisplayed = this.message.notClose
+        }
     }
 
     //Refresh pokemon
     refresh() {
-
+        this.isAnswerDisplayed = false
+        this.messageDisplayed = ""
+        this.inputPokemon = ""
     }
 
     //Show response
     response() {
+        this.isAnswerDisplayed = true
+    }
 
+    openModal() {
+        this.isModalOpen = !this.isModalOpen
     }
 }
---
+++ front/src/app/service/settings/settings.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SettingsService {
+    //Generations
+    gens = { one: true, two: true, three: true, four: true, five: true, six: true, seven: true }
+
+    //Lang
+    lang = "fr"
+
+    constructor() { }
+
+    getSettings() {
+        return {
+            gens: this.gens,
+            lang: this.lang
+        }
+    }
+
+    editLang(newLang) {
+        this.lang = newLang
+    }
+
+    editGens(newGens) {
+        this.gens = newGens
+    }
+}
